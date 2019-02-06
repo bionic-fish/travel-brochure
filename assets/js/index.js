8c98af9c7a03d316baf671cce40b0d4fed7ccdf4
Chris <c.d.harris.dedoc@gmail.com>
2019-02-07 01:40:21 +1100

    push.travel-brochure.html

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+changeColor();
+
+function changeColor() {
+// var headerOrange = document.getElementById('header__orange').style.backgroundColor = 'orange'; 
+// var colorOrange = document.getElementById('color__orange').style.color = 'orange';
+}
+
